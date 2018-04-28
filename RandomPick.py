c52796244f947c80a190d74a354909be5e2da9a4
TastelessTacos <12861387+TastelessTacos@users.noreply.github.com>
2018-04-28 11:40:44 +0300

    Willing to add another sheet to my file, so I updated the variable names accordingly. Also added single quotes around returned titles.

--- RandomPick.py
+++ RandomPick.py
@@ -3,18 +3,18 @@ import random
 import string
 
 file = "list.xlsx"
-workbook = xlrd.open_workbook(file)     # Load the workbook in specified file.
-sheet = workbook.sheet_by_name("list")  # Load the sheet in specified workbook.
-rows = sheet.nrows                      # Counts how many rows in sheet.
+workbook = xlrd.open_workbook(file)                 # Load the workbook in specified file.
+sheetMovies = workbook.sheet_by_name("Movies")      # Load the sheet in specified workbook.
+rows = sheetMovies.nrows                                  # Counts how many rows in sheet.
 
 print("There are currently", rows, "tiles in the list. Let's pick one.")
 
 randomNumber = random.randint(1, rows)              # Create a random number based on total rows in the list.
-randomPick = sheet.cell_value(randomNumber-1, 0)    # Matches the random number with the cell value.
+randomPick = sheetMovies.cell_value(randomNumber-1, 0)    # Matches the random number with the cell value.
 
 if randomPick.istitle == True:
     # Checks if title format is followed.
-    print("How about watching " + randomPick + "?")
+    print("How about watching '" + randomPick + "'?")
 else:
     # If title format was previously not met, it prints it out in a correct title form.
-    print("How about watching " + string.capwords(randomPick) + "?")
+    print("How about watching '" + string.capwords(randomPick) + "'?")
